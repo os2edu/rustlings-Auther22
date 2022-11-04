9339d8d52683f5630f5b2ea30436987e037d4285
Auther22 <3587218818@qq.com>
2022-11-04 14:29:14 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,22 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut a=input.to_string();
+    let mut s=a.trim_matches(' ');
+
+    return s.to_string();
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
-}
+    let mut a=input.to_string();
 
+    let mut s=a+" world!";
+    return s.to_string()
+}
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let s=input.replace("cars","balloons");
+    s
 }
 
 #[cfg(test)]
