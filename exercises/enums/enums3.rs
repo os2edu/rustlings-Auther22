f04180dd53c94bc68db8ce37936eefe32f114d24
Auther22 <3587218818@qq.com>
2022-11-04 12:38:19 +0800

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,16 +2,18 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-enum Message {
-    // TODO: implement the message variant types based on their usage below
-}
 
 struct Point {
     x: u8,
     y: u8,
 }
+enum Message {
+    ChangeColor(u8,u8,u8),
+    Echo(String),
+    Move { x: u8, y: u8},
+    Quit
+}
+
 
 struct State {
     color: (u8, u8, u8),
@@ -37,7 +39,21 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message{
+            Message::Move{x,y}=>{
+                self.position.x=x;
+                self.position.y=y;
+            },
+            Message::ChangeColor(a,b,c)=>{
+                self.color=(a,b,c);
+            },
+            Message::Quit=>{
+                self.quit=true;
+            }
+            _=>{
+
+            }
+        }
     }
 }
 
@@ -52,9 +68,9 @@ mod tests {
             position: Point { x: 0, y: 0 },
             color: (0, 0, 0),
         };
-        state.process(Message::ChangeColor((255, 0, 255)));
+        state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("hello world")));
-        state.process(Message::Move(Point { x: 10, y: 15 }));
+        state.process(Message::Move{ x: 10, y: 15 });
         state.process(Message::Quit);
 
         assert_eq!(state.color, (255, 0, 255));
